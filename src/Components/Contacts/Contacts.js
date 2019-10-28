1adb1e932e5c7fd24a6a8983ac3584c30639cac3
SerejKa-lab <seruogacom@mail.ru>
2019-10-28 20:40:08 +0300

    Contacts, Slogan, MyProject modules corrected

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -2,24 +2,21 @@ import React from "react";
 import styles from "./Contacts.module.css";
 
 function Contacts() {
-  return (
-    <div className={styles.contacts}>
-      <div className={styles.container}>
-        <div className={styles.header}>Контакты</div>
-        <div className={styles.contactsBox}>
-          <p>Имя</p>
-         <input type="text"/>
-         <p>Контакты</p>
-         <input type="text"/>
-         <p>Сообщение</p>
-         <textarea name="" id="" cols="30" rows="10"></textarea>
-        </div>
-        <div className={styles.btn}>
-          <button>Отправить</button>
-        </div>
+   return (
+      <div className={styles.contacts}>
+         <div className={styles.container}>
+            <div className={styles.header}>Контакты</div>
+            <div className={styles.contactsBox}>
+               <input type="text" placeholder='Your name' />
+               <input type="email" placeholder = 'E-mail' />
+               <textarea placeholder = 'Message' cols="30" rows="5"></textarea>
+            </div>
+            <div className={styles.btn}>
+               <button>Отправить</button>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default Contacts;
--- src/Components/MyProjects/MyProjects.js
+++ src/Components/MyProjects/MyProjects.js
@@ -3,14 +3,17 @@ import styles from "./MyProjects.module.css";
 import Project from "./Project/Project";
 
 function MyProjects() {
-  return (
-    <div className={styles.myProjects}>
-      <div className={styles.container}>
-          <Project />
-          <Project />       
-        </div>
-    </div>
-  );
+   return (
+      <div className={styles.myProjects}>
+         <div className = {styles.container} >
+            <span className = {styles.header}>Мои работы</span>
+            <div className={styles.projectsContainer}>
+               <Project />
+               <Project />
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default MyProjects;
