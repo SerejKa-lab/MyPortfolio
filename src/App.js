a6ebfd6fefc96940d2291a87c910eba0fa3d62be
SerejKa-lab <seruogacom@mail.ru>
2019-11-22 15:47:41 +0300

    all components are gather in App instead Portfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import './App.css';
-import Portfolio from './Components/Portfolio';
+import styles from './App.module.css';
+import Header from './Components/Header/Header';
+import Skills from './Components/Skills/Skills';
+import Slogan from './Components/Slogan/Slogan';
+import Contacts from './Components/Contacts/Contacts';
+import Footer from './Components/Footer/Footer';
+import MyProjects from './Components/MyProjects/MyProjects';
+import Biography from './Components/Biography/Biography';
+import Greeting from './Components/Greeting/Greeting';
 
 function App() {
   return (
-    <Portfolio />
-  );
+    <div className={styles.app}>
+      <Header />
+      <Greeting />
+      <Biography />
+      <Skills />
+      <MyProjects />
+      <Slogan />
+      <Contacts />
+      <Footer />
+    </div>
+    );
 }
 
 export default App;
