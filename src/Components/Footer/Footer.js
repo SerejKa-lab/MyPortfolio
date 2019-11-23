fe25bc9953237bfe73bf1edab3d86f19c508e755
SerejKa-lab <seruogacom@mail.ru>
2019-11-23 11:36:44 +0300

    className refactoring for Footer <a> elements

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,10 +9,10 @@ function Footer(props) {
          <div className={styles.container}>
             <span className={styles.name}>Кашко Сергей</span>
             <nav className={styles.linksBox}>
-               <a href='#1' className={styles.link}>1</a>
-               <a href='#2' className={styles.link}>2</a>
-               <a href='#3' className={styles.link}>3</a>
-               <a href='#4' className={styles.link}>4</a>
+               <a href='#1'>1</a>
+               <a href='#2'>2</a>
+               <a href='#3'>3</a>
+               <a href='#4'>4</a>
             </nav>
             <span className={styles.copyright}>Все права защищены</span>
          </div>
