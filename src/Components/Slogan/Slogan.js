21221fa49b4ebfc9e92be8e17ee5d7b044971e0c
SerejKa-lab <seruogacom@mail.ru>
2019-11-23 19:32:59 +0300

    border-radius is set for all Components container backgrounds

--- src/Components/Slogan/Slogan.js
+++ src/Components/Slogan/Slogan.js
@@ -9,7 +9,7 @@ const Slogan = () => {
          <div className={styles.container}>
             <ComponentHeader title = 'Работаю на результат!' />
             <img src="https://newbusiness.su/wp-content/uploads/2019/08/post_5d57cc231e206-695x391.jpeg" alt="Result" />
-            <Button title = 'Предложить работу!' />
+            <Button title = 'Сотрудничать!' />
          </div>
       </div>
    );
