30b8115f1187dc668878f6015d884e314abec6b3
SerejKa-lab <seruogacom@mail.ru>
2019-11-23 10:50:20 +0300

    footerSPA class for spaDisplayMode

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends React.Component {
                   <Contacts />
                </div>
             }
-            <Footer />
+            <Footer spaDisplayMode = {this.state.spaDisplayMode} />
          </div>
       );
    }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,21 +1,23 @@
 import React from "react";
 import styles from "./Footer.module.css";
 
-function Footer() {
-  return (
-    <div className={styles.footer}>
-      <div className={styles.container}>
-        <span className={styles.name}>Кашко Сергей</span>
-        <nav className={styles.linksBox}>
-          <a href = '#1' className={styles.link}>1</a>
-          <a href = '#2' className={styles.link}>2</a>
-          <a href = '#3' className={styles.link}>3</a>
-          <a href = '#4' className={styles.link}>4</a>
-        </nav>
-        <span className={styles.copyright}>Все права защищены</span>
+function Footer(props) {
+   debugger;
+   const footerClassName = props.spaDisplayMode ? `${styles.footer} ${styles.footerSPA}` : styles.footer;
+   return (
+      <div className={footerClassName}>
+         <div className={styles.container}>
+            <span className={styles.name}>Кашко Сергей</span>
+            <nav className={styles.linksBox}>
+               <a href='#1' className={styles.link}>1</a>
+               <a href='#2' className={styles.link}>2</a>
+               <a href='#3' className={styles.link}>3</a>
+               <a href='#4' className={styles.link}>4</a>
+            </nav>
+            <span className={styles.copyright}>Все права защищены</span>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default Footer;
