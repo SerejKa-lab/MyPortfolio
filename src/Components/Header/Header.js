cddae89d534e2ef9c00d155c75f94e0e42803d53
SerejKa-lab <seruogacom@mail.ru>
2019-11-24 13:48:36 +0300

    links are corrected

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,17 +11,17 @@ const Header = (props) => {
    return (
       <div className={styles.header}>
          <div className={styles.container}>
-            <NavLink to = '/'>
+            <NavLink to = '/MyPortfolio'>
                <img src="https://avatars.mds.yandex.net/get-dialogs/1676983/b298e77d9920f3e0409e/orig" 
                   alt="settings" className={styles.ToggleModeIcon} onClick={ onSetClick } />
             </NavLink>
             {props.spaDisplayMode
                ? <nav className={styles.navigation}>
-                  <NavLink activeClassName={styles.active} to='/' exact>Главная</NavLink>
-                  <NavLink activeClassName={styles.active} to='/skills'>Навыки</NavLink>
-                  <NavLink activeClassName={styles.active} to='/projects'>Проекты</NavLink>
-                  <NavLink activeClassName={styles.active} to='/slogan'>Слоган</NavLink>
-                  <NavLink activeClassName={styles.active} to='/contacts'>Контакты</NavLink>
+                  <NavLink activeClassName={styles.active} to='/MyPortfolio' exact>Главная</NavLink>
+                  <NavLink activeClassName={styles.active} to='/MyPortfolio/skills'>Навыки</NavLink>
+                  <NavLink activeClassName={styles.active} to='/MyPortfolio/projects'>Проекты</NavLink>
+                  <NavLink activeClassName={styles.active} to='/MyPortfolio/slogan'>Слоган</NavLink>
+                  <NavLink activeClassName={styles.active} to='/MyPortfolio/contacts'>Контакты</NavLink>
                </nav>
                : <nav className={`${styles.navigation} ${styles.displayHTML}`}>
                   <a href="#startPage">Биография</a>
