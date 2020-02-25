a080114741cb68147ee27b3a91779102e0278f9c
SerejKa-lab <seruogacom@mail.ru>
2020-02-25 22:25:15 +0300

    Biography -> photo added

--- src/Components/Biography/Biography.js
+++ src/Components/Biography/Biography.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { injectIntl, FormattedMessage } from 'react-intl'
 import styles from './Biography.module.css'
 import ComponentHeader from '../ComponentHeader/ComponentHeader'
+import photo from '../../Assets/Images/Photo/photo.jpg'
 
 const Biography = ({ intl }) => {
    return (
@@ -9,11 +10,8 @@ const Biography = ({ intl }) => {
          <div className={styles.container}>
             <ComponentHeader title={intl.formatMessage({ id: 'biography.header' })} />
             <div className={styles.biographyBox}>
-               <img
-                  src="https://avatars.mds.yandex.net/get-pdb/770122/4e5f72f5-a2ea-46d5-9e38-3965a3f34483/s1200"
-                  alt='biography'
-               />
-              <div className={styles.description}>
+               <img src={photo} alt='biography' />
+               <div className={styles.description}>
                   <h2><FormattedMessage id='biography.welcome' /></h2>
                   <p><FormattedMessage id='biography.born' /></p>
                   <p><FormattedMessage id='biography.scool' /></p>
@@ -24,7 +22,7 @@ const Biography = ({ intl }) => {
                   <p><FormattedMessage id='biography.personal_qualities' /></p>
                   <h2><FormattedMessage id='biography.interests' /></h2>
                   <p><FormattedMessage id='biography.self_development' /></p>
-              </div>
+               </div>
             </div>
          </div>
       </div>
