34d119da7f85b58752e0bed9f7fb2b332a77ffcb
SerejKa-lab <seruogacom@mail.ru>
2020-02-25 12:19:15 +0300

    Biography internationalization format

--- src/Components/Biography/Biography.js
+++ src/Components/Biography/Biography.js
@@ -13,7 +13,18 @@ const Biography = ({ intl }) => {
                   src="https://avatars.mds.yandex.net/get-pdb/770122/4e5f72f5-a2ea-46d5-9e38-3965a3f34483/s1200"
                   alt='biography'
                />
-               <p><FormattedMessage id='biography.description' /></p>
+              <div className={styles.description}>
+                  <h2><FormattedMessage id='biography.welcome' /></h2>
+                  <p><FormattedMessage id='biography.born' /></p>
+                  <p><FormattedMessage id='biography.scool' /></p>
+                  <p><FormattedMessage id='biography.university' /></p>
+                  <p><FormattedMessage id='biography.work_ligo' /></p>
+                  <p><FormattedMessage id='biography.work_yandex' /></p>
+                  <h2><FormattedMessage id='biography.personal_qualities.header' /></h2>
+                  <p><FormattedMessage id='biography.personal_qualities' /></p>
+                  <h2><FormattedMessage id='biography.interests' /></h2>
+                  <p><FormattedMessage id='biography.self_development' /></p>
+              </div>
             </div>
          </div>
       </div>
