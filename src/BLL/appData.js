4cab1e60fcfc2b316690fd0cb9171785767ef7a7
SerejKa-lab <seruogacom@mail.ru>
2020-05-20 19:21:29 +0300

    JS, Gulp, Sass skills are added

--- src/BLL/appData.js
+++ src/BLL/appData.js
@@ -4,39 +4,60 @@ import react from '../Assets/Images/Skills/react.png'
 import redux from '../Assets/Images/Skills/redux.png'
 import rest from '../Assets/Images/Skills/rest.png'
 import git from '../Assets/Images/Skills/git.png'
+import sass from '../Assets/Images/Skills/sass.png'
+import gulp from '../Assets/Images/Skills/gulp.png'
+import js from '../Assets/Images/Skills/js.png'
 
- export  const skills = [
-    {
-       subject: "HTML",
-       src: html,
-    },
-    {
-       subject: "CSS",
-       src: css,
-    },
-    {
-       subject: "Git",
-       src: git,
-    },
-    {
-       subject: "React",
-       src: react,
-    },
-    {
-       subject: "redux",
-       src: redux,
-    },
-    {
-       subject: "rest",
-       src: rest,
-    }
- ]
+export const skills = [
+   {
+      subject: "HTML",
+      src: html,
+   },
+   {
+      subject: "CSS",
+      src: css,
+   },
+   {
+      subject: "sass",
+      src: sass,
+   },
+   {
+      subject: "js",
+      src: js,
+   },
+   {
+      subject: "React",
+      src: react,
+   },
+   {
+      subject: "redux",
+      src: redux,
+   },
+   {
+      subject: "gulp",
+      src: gulp,
+   },
+   {
+      subject: "rest",
+      src: rest,
+   },
+   {
+      subject: "Git",
+      src: git,
+   },
+]
 
- export const projects = [
-    { title: 'tasksManager', GHLink: 'https://github.com/s-kashko/TasksManager', 
-       projectLink: 'https://s-kashko.github.io/TasksManager' },
-    { title: 'netWork', GHLink: 'https://github.com/s-kashko/NetWork', 
-       projectLink: 'https://s-kashko.github.io/NetWork' },
-    { title: 'portfolio', GHLink: 'https://github.com/s-kashko/MyPortfolio', 
-       projectLink: 'https://s-kashko.github.io/MyPortfolio' },
- ]
+export const projects = [
+   {
+      title: 'tasksManager', GHLink: 'https://github.com/s-kashko/TasksManager',
+      projectLink: 'https://s-kashko.github.io/TasksManager'
+   },
+   {
+      title: 'netWork', GHLink: 'https://github.com/s-kashko/NetWork',
+      projectLink: 'https://s-kashko.github.io/NetWork'
+   },
+   {
+      title: 'portfolio', GHLink: 'https://github.com/s-kashko/MyPortfolio',
+      projectLink: 'https://s-kashko.github.io/MyPortfolio'
+   },
+]
